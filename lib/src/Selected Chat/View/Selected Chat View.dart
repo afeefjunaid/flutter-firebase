f384061839ee063fc90e3ff774fb4517153966de
afeefjunaid <afeef.junaid@gmail.com>
2024-09-10 18:10:52 +0500

    latest code

--- lib/src/Selected Chat/View/Selected Chat View.dart
+++ lib/src/Selected Chat/View/Selected Chat View.dart
@@ -1,7 +1,9 @@
-import 'package:chatapp/src/Base%20Scaffold/View/Base%20Scaffold%20View.dart';
+import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import '../../Base Scaffold/View/Base Scaffold View.dart';
 import '../../Chat_Service/view Model/Chat_Service View Model.dart';
 import '../../Common Widgets/View/Common Widgets View.dart';
 import '../../Notifications/View Model/Notifications View Model.dart';
@@ -15,10 +17,45 @@ class selectedChatView extends StatefulWidget {
 }
 
 class _selectedChatViewState extends State<selectedChatView> {
+
   TextEditingController messageController = TextEditingController();
   FirebaseAuth firebaseAuthinst = FirebaseAuth.instance;
   late ScrollController scrollController;
   chatServiceViewModel chatService = chatServiceViewModel();
+  late FirebaseMessaging messaging;
+
+  late String recordedFilePath;
+
+  @override
+  void initState() {
+    super.initState();
+    requestMicrophonePermission();
+    scrollController = ScrollController();
+    initializeFirebaseMessaging();
+
+
+  }
+
+
+
+
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    messageController.dispose();
+
+    super.dispose();
+  }
+
+  Future<bool> requestMicrophonePermission() async {
+    var status = await Permission.microphone.status;
+    if (!status.isGranted) {
+      status = await Permission.microphone.request();
+    }
+    return status.isGranted;
+  }
+
 
   sendMessage() async {
     if (messageController.text.isNotEmpty) {
@@ -36,24 +73,17 @@ class _selectedChatViewState extends State<selectedChatView> {
   }
 
   void scrollToBottom() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (scrollController.hasClients) {
-        scrollController.animateTo(
-          scrollController.position.maxScrollExtent,
-          duration: Duration(milliseconds: 10),
-          curve: Curves.easeOut,
-        );
-      }
-    });
+    if (scrollController.hasClients) {
+      scrollController.animateTo(
+        scrollController.position.maxScrollExtent,
+        duration: Duration(milliseconds: 1),
+        curve: Curves.easeOut,
+      );
+    }
   }
 
-  late FirebaseMessaging messaging;
-  @override
-  void initState() {
-    super.initState();
-    scrollController = ScrollController();
-    initializeFirebaseMessaging();
-  }
+
+
 
   void initializeFirebaseMessaging() {
     messaging = FirebaseMessaging.instance;
@@ -93,17 +123,13 @@ class _selectedChatViewState extends State<selectedChatView> {
     });
   }
 
-  @override
-  void dispose() {
-    scrollController.dispose();
-    messageController.dispose();
-    super.dispose();
-  }
+
+
+
 
   buildMessageList() {
     return StreamBuilder(
-        stream: chatService.getMessages(
-            widget.selectedUser['uid'], firebaseAuthinst.currentUser!.uid),
+        stream: chatService.getMessages(widget.selectedUser['uid'], firebaseAuthinst.currentUser!.uid),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             return Text("Error ${snapshot.error}");
@@ -111,52 +137,57 @@ class _selectedChatViewState extends State<selectedChatView> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           }
-          WidgetsBinding.instance.addPostFrameCallback((_) => scrollToBottom());
+
+          if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              scrollToBottom();
+            });
+          }
+
           return ListView.builder(
-              controller: scrollController,
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (cnt, index) {
-                var allign = (snapshot.data!.docs[index]["senderId"] ==
-                        firebaseAuthinst.currentUser?.uid)
-                    ? MainAxisAlignment.end
-                    : MainAxisAlignment.start;
-                var bgclr = (snapshot.data!.docs[index]["senderId"] ==
-                        firebaseAuthinst.currentUser?.uid)
-                    ? Color(0xff007665)
-                    : Colors.grey;
-                return Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: allign,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 5),
-                      child: Container(
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          color: bgclr, // Background color
-                          borderRadius: BorderRadius.circular(8.0),
+            controller: scrollController,
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (cnt, index) {
+              var align = (snapshot.data!.docs[index]["senderId"] == firebaseAuthinst.currentUser?.uid)
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start;
+              var bgclr = (snapshot.data!.docs[index]["senderId"] == firebaseAuthinst.currentUser?.uid)
+                  ? Color(0xff007665)
+                  : Colors.grey;
+
+              return Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: align,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: bgclr,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          maxWidth: 200,
                         ),
-                        child: ConstrainedBox(
-                          constraints: BoxConstraints(
-                            maxWidth: 200,
-                          ),
-                          child: Wrap(
-                            children: [
-                              Text(
-                                snapshot.data!.docs[index]["message"],
-                                style:
-                                TextStyle(fontSize: 20, color: Colors.white),
-                              )
-                            ],
-                          ),
+                        child: Wrap(
+                          children: [
+                            Text(
+                              snapshot.data!.docs[index]["message"],
+                              style: TextStyle(fontSize: 20, color: Colors.white),
+                            )
+                          ],
                         ),
                       ),
                     ),
-
-                  ],
-                );
-              });
-        });
+                  ),
+                ],
+              );
+            },
+          );
+        }
+    );
   }
 
   @override
@@ -199,11 +230,20 @@ class _selectedChatViewState extends State<selectedChatView> {
             child: textFormFieldWidget(
                 messageController, "Enter Your Message", "Message"),
           ),
-          IconButton(
-            onPressed: () async {
-              sendMessage();
-            },
-            icon: Icon(Icons.send),
+          Row(
+            children: [
+              GestureDetector(
+
+
+                child: Icon(Icons.mic),
+              ),
+              IconButton(
+                onPressed: () async {
+                  sendMessage();
+                },
+                icon: Icon(Icons.send),
+              ),
+            ],
           )
         ])
       ]),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import cloud_firestore
 import firebase_analytics
 import firebase_auth
@@ -12,8 +13,12 @@ import firebase_core
 import firebase_messaging
 import firebase_storage
 import flutter_local_notifications
+import just_audio
+import path_provider_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
@@ -21,4 +26,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
